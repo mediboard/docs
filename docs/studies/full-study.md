4dab9a52a448fe6a2d1bffe9450491b9b9ff8b54
Porter Hunley <porterehunley@gmail.com>
2023-03-01 15:30:06 -0800

    add in links to github

--- docs/studies/full-study.md
+++ docs/studies/full-study.md
@@ -19,9 +19,50 @@ Gather all data from a study
 
 ## Examples
 
-```jsx title="GET https://api.mediboard.fyi/treatments/search?q=ambien"
-treatments: [
-  {
-    id: 
-  }
-]
+```jsx title="GET https://api.mediboard.fyi/api/studies/1234"
+{
+    "studies": [
+        {
+            "completion_date": "2010-06-01",
+            "conditions": [
+                {
+                    "condition_group": null,
+                    "id": 1044,
+                    "name": "Japanese Encephalitis"
+                }
+            ],
+            "description": "The objective is to assess immunogenicity of a commercial IC51 batch at 3 different time points post filling (12, 18, 24 months) in terms of Geometric Mean Titers (GMT) for anti-JEV neutralizing antibodies at Day 56 after the first vaccination.",
+            "external_ids": [
+                "NCT00776230"
+            ],
+            "gender": "gender.ALL",
+            "id": 1239,
+            "intervention_type": "intervention_type.SINGLE_GROUP_ASSIGNMENT",
+            "max_age": -1,
+            "min_age": 18,
+            "min_age_units": "max_age_units.NA",
+            "official_title": "Immunogenicity of a Commercial Batch of the Japanese Encephalitis Vaccine IC51 up to Twenty-four Months Post Filling. An Open-label, Multicenter, Phase 3 Study",
+            "phase": "Phase 3",
+            "purpose": "Prevention",
+            "responsible_party": "NA",
+            "results_summary": null,
+            "short_title": "Immunogenicity of a Commercial Batch of JEV IC51 up to 24 Months Post Filling",
+            "sponsor": "Valneva Austria GmbH",
+            "status": "Completed",
+            "stopped_reason": "NA",
+            "treatments": [
+                {
+                    "description": null,
+                    "from_study": null,
+                    "id": 15,
+                    "name": "vaccines",
+                    "no_studies": null,
+                    "treatment_group": null
+                }
+            ],
+            "type": "Interventional",
+            "upload_date": "Tue, 01 Mar 2016 00:00:00 GMT"
+        }
+    ]
+}
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'The Medical Board',
+  tagline: 'A modern, open platform for medical data',
   url: 'https://docs.mediboard.fyi',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mediboard', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -59,7 +59,7 @@ const config = {
         },
         items: [
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/mediboard/docs',
             label: 'GitHub',
             position: 'right',
           },
@@ -77,34 +77,8 @@ const config = {
               },
             ],
           },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {              
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} The Medical Board, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
